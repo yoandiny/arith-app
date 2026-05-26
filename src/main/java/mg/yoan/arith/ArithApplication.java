5895c5459d5e1dbb8dffc33f92f0926069c1964e
Yoan <yoandiny14@gmail.com>
2026-05-26 11:25:04 +0300

    Initialize Maven project with Spring Boot setup, including wrapper scripts, POM file, and application structure.

---
+++ src/main/java/mg/yoan/arith/ArithApplication.java
@@ -0,0 +1,13 @@
+package mg.yoan.arith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/yoan/arith/ArithApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.yoan.arith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
